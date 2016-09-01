7214023a37ccfe17d9320d630c28197e11bf8fdc
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-01 11:09:07 -0500

    turn everything lower case

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 function palindrome(str) {
-  // \W removes all non-alphanumeric characters:
-  // \W is equivalent to [^A-Za-z0–9_]
-  // g flag for global search
-  var newStr = str.replace( /[^A-Za-z0–9]/g);
+/*The RegExp constructor creates a regular expression object for matching text with a pattern.
+The replace() method returns a new string with some or all matches of a pattern
+replaced by a replacement. The pattern can be a string or a RegExp, and the
+replacement can be a string or a function to be called for each match.
+
+str.replace(regexp|substr, newSubStr|function)*/
+var re = /[^A-Za-z0-9]/g;
+
+var lowRegStr = str.toLowerCase().replace(re, '');
+console.log(lowRegStr);
   return true;
 }
 
-
-
-palindrome("eye");
+palindrome("_Eye");
+palindrome("race car");
+palindrome("A man, a plan, a canal. Panama");
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
